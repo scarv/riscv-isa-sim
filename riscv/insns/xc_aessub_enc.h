9dff12e888e512465f2d4c8d309699ec03643ff4
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-02-11 09:33:57 +0000

    SCARV: Implement xc.aessub.* instructions.
     On branch dev/xcrypto
     Changes to be committed:
    	new file:   ../riscv/insns/xc_aes_common.h
    	new file:   ../riscv/insns/xc_aessub_dec.h
    	new file:   ../riscv/insns/xc_aessub_decrot.h
    	new file:   ../riscv/insns/xc_aessub_enc.h
    	new file:   ../riscv/insns/xc_aessub_encrot.h
    	modified:   ../riscv/riscv.mk.in

---
+++ riscv/insns/xc_aessub_enc.h
@@ -0,0 +1,13 @@
+require_extension('x');
+
+#include "xc_aes_common.h"
+
+AES_UNPACK_BYTES(b0, b1, b2, b3);
+
+// Forward SBOX
+AES_SBOX(b0, b1, b2, b3);
+
+uint32_t result = AES_PACK_BYTES(b0, b1, b2, b3);
+
+WRITE_XCRD(result);
+
