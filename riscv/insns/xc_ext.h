79dcd7169ec8edb6fec249bdd00d4e0869ffe2fa
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-03-29 09:48:28 +0000

    Somehow missed including ins/ext in the makefile...
    - Also fixed their implementation.
    - See scarv/xcrypto-spec#14
    
     On branch dev/ins-ext
     Changes to be committed:
    	modified:   ../riscv/insns/xc_ext.h
    	modified:   ../riscv/insns/xc_ins.h
    	modified:   ../riscv/riscv.mk.in

--- riscv/insns/xc_ext.h
+++ riscv/insns/xc_ext.h
@@ -1,7 +1,8 @@
 require_extension('x');
 
-uint32_t cs = insn.xc_cs()     ;
+uint32_t cs = insn.xc_cs();
 uint32_t cl = insn.xc_cl();
+uint32_t len = cl + 1     ;
 
 if((cs + cl) >= 32) {
 
@@ -9,8 +10,11 @@ if((cs + cl) >= 32) {
 
 } else {
     
-    uint32_t mask   = ~(0xFFFFFFFF << (cl+1));
+    uint32_t mask   = len == 32 ? 0xFFFFFFFF : ~(0xFFFFFFFF << len);
     uint32_t wdata  = (XCRS1 >> cs) & mask;
+    
+    //printf("# xc.ext %d, %d, %08X, %08X\n",cs,len,XCRS1, wdata);
+    //fflush(stdout);
 
     WRITE_XCRD(wdata);
 
--- riscv/insns/xc_ins.h
+++ riscv/insns/xc_ins.h
@@ -3,6 +3,7 @@ require_extension('x');
 uint32_t cs = insn.xc_cs();
 uint32_t cl = insn.xc_cl();
 uint32_t end= cs + cl     ;
+uint32_t len= cl + 1      ;
 
 if(end >= 32) {
     
@@ -10,9 +11,12 @@ if(end >= 32) {
 
 } else {
 
-    uint32_t mask = ~(0xFFFFFFFF << (cl+1));
+    uint32_t mask   = len == 32 ? 0xFFFFFFFF : ~(0xFFFFFFFF << len);
     uint32_t src  = XCRS1 & mask;
-    uint32_t wdata= (XCRD & ~(mask << cs)) | (mask << cs);
+    uint32_t wdata= (XCRD & ~(mask << cs)) | (src << cs);
+
+    //printf("# xc.ins %d, %d, %08X, %08X\n",cs,len,XCRS1, wdata);
+    //fflush(stdout);
 
     WRITE_XCRD(wdata);
 
