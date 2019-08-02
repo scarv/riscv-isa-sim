5ec62e4f6b978de3562387f407a12c811727e272
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-08-02 14:45:28 +0100

    SCARV: Fix xc.mror implementation.
     On branch scarv/xcrypto/v1.0.0
     Your branch is up-to-date with 'origin/scarv/xcrypto/v1.0.0'.
    
     Changes to be committed:
    	modified:   ../riscv/insns/xc_mror.h

--- riscv/insns/xc_mror.h
+++ riscv/insns/xc_mror.h
@@ -4,11 +4,19 @@ int rd2 = (insn.xc_rdm() << 1) | 0x1 ;
 
 int ramt= RS3 & 0x3F;
     
-uint64_t result_lo;
-uint64_t result_hi;
+uint32_t result_lo;
+uint32_t result_hi;
 
-uint64_t rot_in = ((uint64_t)RS1<<32) | RS2;
+uint64_t rs1 = RS1 & 0xFFFFFFFF;
+uint64_t rs2 = RS2 & 0xFFFFFFFF;
+
+uint64_t rot_in = (((uint64_t) rs1) << 32) | ((uint64_t) rs2);
 uint64_t result = (rot_in >> ramt) | (rot_in << (64-ramt));
+
+//printf("# RS1 = %08lx, %08lx\n", RS1, rs1);
+//printf("# RS2 = %08lx, %08lx\n", RS2, rs2);
+//printf("#(%d,%d) %016lx >>> %d = %016lx\n",rd2,rd1,rot_in,ramt,result);
+//fflush(stdout);
 
 result_lo       = result & 0xFFFFFFFF;
 result_hi       = result >> xlen     ;
