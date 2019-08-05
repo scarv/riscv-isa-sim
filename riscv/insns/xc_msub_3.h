a1aa44969bf769df25c87919ad1b0fa052d8659b
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-08-05 10:17:05 +0100

    SCARV: Fix xc.madd.3 and xc.msub.3
     On branch scarv/xcrypto/v1.0.0
     Your branch is up-to-date with 'origin/scarv/xcrypto/v1.0.0'.
    
     Changes to be committed:
    	modified:   ../riscv/insns/xc_madd_3.h
    	modified:   ../riscv/insns/xc_msub_3.h

--- riscv/insns/xc_msub_3.h
+++ riscv/insns/xc_msub_3.h
@@ -1,29 +1,33 @@
 
 int rd1 = (insn.xc_rdm() << 1)       ;
 int rd2 = (insn.xc_rdm() << 1) | 0x1 ;
-    
-uint64_t result_lo;
-uint64_t result_hi;
 
 if(xlen == 32) {
 
-    uint64_t result = RS1 - RS2 - (RS3 & 0x1);
+    uint64_t    result      = (uint64_t)(RS1 & 0xFFFFFFFF) - 
+                              (uint64_t)(RS2 & 0xFFFFFFFF) - 
+                              (uint64_t)(RS3 & 0x1);
+    uint32_t    result_lo   =       (uint32_t)(result      );
+    uint32_t    result_hi   = 0x1 & (uint32_t)(result >> 32);
 
-    result_lo       = result & 0xFFFFFFFF;
-    result_hi       = result >> xlen     ;
+    WRITE_REG(rd1, result_lo);
+    WRITE_REG(rd2, result_hi);
 
 } else if(xlen == 64) {
 
-    int128_t result = (int128_t)RS1 - RS2 - (RS3&0x1);
+    uint128_t   result      = (uint128_t)(RS1 & 0xFFFFFFFFFFFFFFFF) - 
+                              (uint128_t)(RS2 & 0xFFFFFFFFFFFFFFFF) - 
+                              (uint128_t)(RS3 & 0x1);
+    uint64_t    result_lo   =       (uint64_t)(result      );
+    uint64_t    result_hi   = 0x1 & (uint64_t)(result >> 64);
 
-    result_lo = result & 0xFFFFFFFFFFFFFFFF;
-    result_hi = (result >> xlen);
+    WRITE_REG(rd1, result_lo);
+    WRITE_REG(rd2, result_hi);
 
 } else {
     // TBD: How do these instructions work on RV128?
     assert(0);
 }
 
-WRITE_REG(rd1, result_lo);
-WRITE_REG(rd2, result_hi);
+
 
