16609dbb72068cda461767a0c9afb155f1beac10
Ben Marshall <ben.marshall@bristol.ac.uk>
2019-08-05 13:19:44 +0100

    SCARV: Fix xc.lut
     On branch scarv/xcrypto/v1.0.0
     Your branch is up-to-date with 'origin/scarv/xcrypto/v1.0.0'.
    
     Changes to be committed:
    	modified:   ../riscv/insns/xc_lut.h

--- riscv/insns/xc_lut.h
+++ riscv/insns/xc_lut.h
@@ -1,14 +1,16 @@
 
-// Concatenate RS3 || RS2
-uint64_t lut = ((uint64_t)RS3 << 32) | RS2;
+// Concatenate RS1 || RS2
+uint64_t lut = (((uint64_t)RS1 & 0xFFFFFFFF) << 32) | (RS2&0xFFFFFFFF);
 
 uint32_t wdata = 0;
+
+uint32_t rd = RD;
 
 // For each nibble in rs1
 for(int i = 0; i < 8; i ++) {
 
     // 0 <= to_sub < 16 - the i'th nibble
-    uint8_t to_sub = (RS1 >> (4*i)) & 0xF;
+    uint8_t to_sub = (rd >> (4*i)) & 0xF;
     
     // Index into the LUT.
     uint32_t subd  = (lut >> (to_sub*4)) & 0xF;
